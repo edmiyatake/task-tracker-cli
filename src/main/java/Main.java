2444a123c4f1f18fbc7065347cd0a42d3cb8b4cd
Edwin Miyatake <edmiyatake@gmail.com>
2026-03-10 10:31:49 -0700

    implemented input parameters

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Task Tracker CLI!");
+
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("args[" + i + "] = " + args[i]);
+        }
     }
 }
